49218e11fe5f9eab7b361e18fdf53019e1098974
Alaa Mohamed Morsy <62466931+Alaa-Morsy@users.noreply.github.com>
2022-03-21 14:25:30 +0200

    Update LCD_test.c

--- Projects/LCD_test.c
+++ Projects/LCD_test.c
@@ -2,32 +2,32 @@
 // * LCD_test.c
 // *
 // *  Created on: Sep 17, 2021
-// *      Author: Arabtech
+// *      Author: Alaa-Morsy
 // */
-//#include "util/delay.h"
-//#include "../LIB/STD_TYPES.h"
-//#include "../HAL/LCD_Handler/LCD_int.h"
-//
-//void main(void)
-//{
-//	LCD_vidInit ();
-//	u8 i;
-//	u8 Name = "ALAA";
-//	while (1)
-//	{
-//		LCD_vidReturnHome();
-//		LCD_vidSendString (Name, 4);
-//		for(i=0 ; i<12 ; i++)
-//		{
-//			LCD_vidShiftRight();
-//			_delay_ms(250);
-//		}
-//		_delay_ms(1000);
-//		for(i=0 ; i<12 ; i++)
-//		{
-//			LCD_vidShiftLeft();
-//			_delay_ms(250);
-//		}
-//		_delay_ms(1000);
-//	}
-//}
+#include "util/delay.h"
+#include "../LIB/STD_TYPES.h"
+#include "../HAL/LCD_Handler/LCD_int.h"
+
+void main(void)
+{
+	LCD_vidInit ();
+	u8 i;
+	u8 Name = "ALAA";
+	while (1)
+	{
+		LCD_vidReturnHome();
+		LCD_vidSendString (Name, 4);
+		for(i=0 ; i<12 ; i++)
+		{
+			LCD_vidShiftRight();
+			_delay_ms(250);
+		}
+		_delay_ms(1000);
+		for(i=0 ; i<12 ; i++)
+		{
+			LCD_vidShiftLeft();
+			_delay_ms(250);
+		}
+		_delay_ms(1000);
+	}
+}
